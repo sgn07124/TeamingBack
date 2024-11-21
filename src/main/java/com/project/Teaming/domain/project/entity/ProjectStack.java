9e1a8aa076f54b3cf2259fd8c3c3d578d1ff2213
Kim Kyeongho <sgn07124@gmail.com>
2024-11-21 23:51:16 +0900

    feat: 기술 스택 db에 추가 #61
    DB에 기술 스택에 대한 데이터가 없으면 프로젝트 실행 시 추가되는 기능 추가.
    추후 추가적인 기술 스택들 추가 예정

--- src/main/java/com/project/Teaming/domain/project/entity/ProjectStack.java
+++
@@ -1,8 +0,0 @@
-package com.project.Teaming.domain.project.entity;
-
-public enum ProjectStack {
-    JavaScript,TypeScript,React,Vue,Nodejs,Spring,Java,Nextjs,Nestjs,
-    Express,Go,C,Python,Django,Swift,Kotlin,MySql,MongoDB,php,GraphQL,
-    FireBase,ReactNative,Unity,Flutter,AWS,Kubernetes,Docker,Git,Figma,
-    Zeplin
-}
--- src/main/java/com/project/Teaming/domain/project/entity/Stack.java
+++ src/main/java/com/project/Teaming/domain/project/entity/Stack.java
@@ -19,11 +19,15 @@ public class Stack {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "stack_id")
     private Long id;
-    @Enumerated(EnumType.STRING)
+
     @Column(name = "stack_name",unique = true)
-    private ProjectStack stackName;
+    private String stackName;
     @OneToMany(mappedBy = "stack")
     private List<TeamStack> teamStacks = new ArrayList<>();
     @OneToMany(mappedBy = "stack")
     private List<UserStack> userStacks = new ArrayList<>();
+
+    public Stack(String stackName) {
+        this.stackName = stackName;
+    }
 }
