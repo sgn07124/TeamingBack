d65e91710903c046a511773691c8679e72892db6
Kim Kyeongho <sgn07124@gmail.com>
2024-12-01 21:02:13 +0900

    fix: 응답 객체 수정 - Project

--- src/main/java/com/project/Teaming/domain/project/controller/ProjectParticipateController.java
+++ src/main/java/com/project/Teaming/domain/project/controller/ProjectParticipateController.java
@@ -1,10 +1,10 @@
 package com.project.Teaming.domain.project.controller;
 
 import com.project.Teaming.domain.project.dto.response.ProjectParticipationInfoDto;
-import com.project.Teaming.domain.project.entity.ProjectParticipation;
 import com.project.Teaming.domain.project.service.ProjectParticipationService;
 import com.project.Teaming.global.result.ResultCode;
-import com.project.Teaming.global.result.ResultResponse;
+import com.project.Teaming.global.result.ResultDetailResponse;
+import com.project.Teaming.global.result.ResultListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
@@ -27,43 +27,43 @@ public class ProjectParticipateController {
 
     @PostMapping("/project/join/{team_id}")
     @Operation(summary = "프로젝트 팀에 신청", description = "팀원으로 들어가길 원하는 사용자가 팀에 신청을 하면 대기열에 등록된다.")
-    public ResultResponse<Void> joinProjectTeam(@PathVariable Long team_id) {
+    public ResultDetailResponse<Void> joinProjectTeam(@PathVariable Long team_id) {
         projectParticipationService.joinTeam(team_id);
-        return new ResultResponse<>(ResultCode.JOIN_MEMBER_PROJECT_TEAM, null);
+        return new ResultDetailResponse<>(ResultCode.JOIN_MEMBER_PROJECT_TEAM, null);
     }
 
     @DeleteMapping("/project/join/{team_id}/cancel")
     @Operation(summary = "프로젝트 신청 취소", description = "대기열에 있는 팀원은 원할 때, 신청 취소(status: pending, isDeleted: false 인 경우에만 가능)를 할 수 있다.")
-    public ResultResponse<Void> cancelProjectTeam(@PathVariable Long team_id) {
+    public ResultDetailResponse<Void> cancelProjectTeam(@PathVariable Long team_id) {
         projectParticipationService.cancelTeam(team_id);
-        return new ResultResponse<>(ResultCode.CANCEL_PROJECT_TEAM, null);
+        return new ResultDetailResponse<>(ResultCode.CANCEL_PROJECT_TEAM, null);
     }
 
     @PutMapping("/project/{team_id}/quit")
     @Operation(summary = "프로젝트 탈퇴", description = "프로젝트 팀의 정식 팀원이 팀에서 스스로 탈퇴할 수 있다.(만약 무단으로 나가는 경우 추후에 팀원들에게 신고를 당할 수 있다.)")
-    public ResultResponse<Void> quitProjectTeam(@PathVariable Long team_id) {
+    public ResultDetailResponse<Void> quitProjectTeam(@PathVariable Long team_id) {
         projectParticipationService.quitTeam(team_id);
-        return new ResultResponse<>(ResultCode.QUIT_PROJECT_TEAM, null);
+        return new ResultDetailResponse<>(ResultCode.QUIT_PROJECT_TEAM, null);
     }
 
     @PutMapping("/project/team/{team_id}/{user_id}/accept")
     @Operation(summary = "팀장의 팀원 신청 수락", description = "프로젝트 팀의 팀장은 대기열의 팀원 신청에 대하여 수락을 한다.")
-    public ResultResponse<Void> acceptedTeamMember(@PathVariable Long team_id, @PathVariable Long user_id) {
+    public ResultDetailResponse<Void> acceptedTeamMember(@PathVariable Long team_id, @PathVariable Long user_id) {
         projectParticipationService.acceptedMember(team_id, user_id);
-        return new ResultResponse<>(ResultCode.ACCEPT_JOIN_MEMBER, null);
+        return new ResultDetailResponse<>(ResultCode.ACCEPT_JOIN_MEMBER, null);
     }
 
     @PutMapping("/project/team/{team_id}/{user_id}/reject")
     @Operation(summary = "팀장의 팀원 신청 거절", description = "프로젝트 팀의 팀장은 대기열의 팀원 신청에 대하여 거절을 한다.")
-    public ResultResponse<Void> rejectedTeamMember(@PathVariable Long team_id, @PathVariable Long user_id) {
+    public ResultDetailResponse<Void> rejectedTeamMember(@PathVariable Long team_id, @PathVariable Long user_id) {
         projectParticipationService.rejectedMember(team_id, user_id);
-        return new ResultResponse<>(ResultCode.REJECT_JOIN_MEMBER, null);
+        return new ResultDetailResponse<>(ResultCode.REJECT_JOIN_MEMBER, null);
     }
 
     @GetMapping("/project/team/{team_id}/participations")
     @Operation(summary = "프로젝트 팀 지원자 목록(대기열) 조회", description = "해당 프로젝트 팀의 지원자들을 조회한다.")
-    public ResultResponse<ProjectParticipationInfoDto> getParticipations(@PathVariable Long team_id){
+    public ResultListResponse<ProjectParticipationInfoDto> getParticipations(@PathVariable Long team_id){
         List<ProjectParticipationInfoDto> list = projectParticipationService.getAllParticipationDtos(team_id);
-        return  new ResultResponse<>(ResultCode.GET_PARTICIPATION_LIST, list);
+        return  new ResultListResponse<>(ResultCode.GET_PARTICIPATION_LIST, list);
     }
 }
