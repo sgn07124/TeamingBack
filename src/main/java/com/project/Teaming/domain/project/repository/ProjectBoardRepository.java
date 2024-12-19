41e7446ea50bebf0f02e2e59b48c399ecdfd3b70
Kim Kyeongho <sgn07124@gmail.com>
2024-12-20 01:57:21 +0900

    feat: 게시물 전체 조회 Api 로직 수정 #141
    - 무한스크롤 적용을 위해 Offset-based Pagination → Cursor-based Pagination 으로 수정
    - 페이지 기본값: 10

--- src/main/java/com/project/Teaming/domain/project/repository/ProjectBoardRepository.java
+++ src/main/java/com/project/Teaming/domain/project/repository/ProjectBoardRepository.java
@@ -3,7 +3,6 @@ package com.project.Teaming.domain.project.repository;
 import com.project.Teaming.domain.project.entity.PostStatus;
 import com.project.Teaming.domain.project.entity.ProjectBoard;
 import java.util.List;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,14 +10,14 @@ import org.springframework.data.repository.query.Param;
 
 public interface ProjectBoardRepository extends JpaRepository<ProjectBoard, Long> {
 
-    @Query("SELECT pb FROM ProjectBoard  pb "
-            + "JOIN FETCH pb.projectTeam pt "
-            + "WHERE (:status IS NULL OR pb.status = :status) "
-            + "ORDER BY pb.createdDate DESC ")
-    Page<ProjectBoard> findAllByStatusOptional(@Param("status")PostStatus status, Pageable pageable);
-
     @Query("SELECT pb FROM ProjectBoard pb WHERE pb.projectTeam.id = :teamId ORDER BY pb.createdDate DESC ")
     List<ProjectBoard> findAllByProjectTeamId(@Param("teamId") Long teamId);
 
     List<ProjectBoard> findAllByStatus(PostStatus status);
+
+    @Query("SELECT pb FROM ProjectBoard pb "
+            + "JOIN FETCH pb.projectTeam pt "
+            + "WHERE (:cursor IS NULL OR pb.id <= :cursor) "
+            + "ORDER BY pb.createdDate DESC ")
+    List<ProjectBoard> findAllByCursor(@Param("cursor") Long cursor, Pageable pageable);
 }
