48acfa332354e32f65fd4f5ddfb63c73058936d0
minseo <gwonm305@gmail.com>
2024-12-26 14:44:46 +0900

    feat: MentoringStatus 상태 자동 업데이트 로직 추가 #139

--- src/main/java/com/project/Teaming/TeamingApplication.java
+++ src/main/java/com/project/Teaming/TeamingApplication.java
@@ -5,9 +5,11 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 @OpenAPIDefinition(servers = {@Server(url = "https://myspringserver.shop", description = "teaming server")})
 public class TeamingApplication {
 
