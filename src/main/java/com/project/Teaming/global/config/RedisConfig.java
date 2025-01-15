6ca58cf38fa996e122e12ececeab78b0da79c1dc
Kim Kyeongho <sgn07124@gmail.com>
2025-01-15 22:39:59 +0900

    refactor: 팀 게시글 무한 스크롤 조회 시 조회 성능 개선 #186
    - 데이터 조회 시, Redis에 데이터가 없다면 DB에서 커서 기반 페이징 단위로 Redis에 저장
    - Redis에 데이터가 있다면 Redis에서 데이터 조회.
    - Jitter 개념을 적용하여 TTL 만료에 대한 DB 조회 시, Cache Miss 문제 해결

--- src/main/java/com/project/Teaming/global/config/RedisConfig.java
+++ src/main/java/com/project/Teaming/global/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.project.Teaming.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,8 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -16,19 +19,23 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisConfig {
 
     private final RedisProperties redisProperties;
+    @Value("${spring.data.redis.host}") String host;
 
+    // 기본 db(0번)용 ConnectionFactory
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisProperties.getHost());
         redisStandaloneConfiguration.setPort(redisProperties.getPort());
         redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        redisStandaloneConfiguration.setDatabase(0);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
+    // 기본 db(0번)용 RedisTemplate
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
-
+        // 0번 DB(기본값) 설정
         // redisTemplate 를 받아와서 set, get, delete 를 사용
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         /*
@@ -38,5 +45,30 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         return redisTemplate;
+    }
+
+    // 게시글 db(1번)용 ConnectionFactory
+    @Bean
+    public RedisConnectionFactory postRedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        redisStandaloneConfiguration.setDatabase(1);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    // 게시글 db(1번)용 RedisTemplate
+    @Bean
+    public RedisTemplate<String, Object> postRedisTemplate() {
+        // 1번 DB에서 사용할 RedisTemplate 설정
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(postRedisConnectionFactory());
+
+        // 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
     }
 }
