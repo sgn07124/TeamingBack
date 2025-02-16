586ff36c73d02389305a15645dd7e515e41adb84
Kim Kyeongho <sgn07124@gmail.com>
2025-02-17 00:30:52 +0900

    fix: 알림 내역 전체 조회 Api 수정 #186
    - 팀 id 추가
    - 날짜 포맷 설정

--- src/main/java/com/project/Teaming/global/sse/dto/NotificationResponseDto.java
+++ src/main/java/com/project/Teaming/global/sse/dto/NotificationResponseDto.java
@@ -1,6 +1,8 @@
 package com.project.Teaming.global.sse.dto;
 
 import com.project.Teaming.global.sse.entity.Notification;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ public class NotificationResponseDto {
     private Long userId;
     private String message;
     private String type;
+    private Long teamId;
     private boolean isRead;
     private String createdAt;
 
@@ -23,8 +26,14 @@ public class NotificationResponseDto {
         dto.setUserId(notification.getUser().getId());
         dto.setMessage(notification.getMessage());
         dto.setType(notification.getType());
+        dto.setTeamId(notification.getTeamId());
         dto.setRead(notification.isRead());
-        dto.setCreatedAt(notification.getCreatedAt().toString());
+        dto.setCreatedAt(dto.getFormattedDate(notification.getCreatedAt()));
         return dto;
+    }
+
+    public String getFormattedDate(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return dateTime.format(formatter);
     }
 }
--- src/main/java/com/project/Teaming/global/sse/repository/NotificationRepository.java
+++ src/main/java/com/project/Teaming/global/sse/repository/NotificationRepository.java
@@ -2,8 +2,9 @@ package com.project.Teaming.global.sse.repository;
 
 import com.project.Teaming.global.sse.entity.Notification;
 import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUserId(Long userId);
+    List<Notification> findByUserId(Long userId, Sort sort);
 }
