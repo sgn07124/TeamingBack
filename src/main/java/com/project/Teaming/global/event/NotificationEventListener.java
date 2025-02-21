461cce69dce17c95ffc72b190f1273b2efbe64d8
minseo <gwonm305@gmail.com>
2025-02-21 13:58:40 +0900

    refactor: kafka 이용해 알림 서버에 분산 적용 되도록 수정 #198

--- src/main/java/com/project/Teaming/global/event/NotificationEventListener.java
+++ src/main/java/com/project/Teaming/global/event/NotificationEventListener.java
@@ -1,96 +1,25 @@
 package com.project.Teaming.global.event;
 
-import com.project.Teaming.global.sse.dto.EventPayload;
-import com.project.Teaming.global.sse.dto.EventWithTeamPayload;
-import com.project.Teaming.global.sse.entity.Notification;
-import com.project.Teaming.global.sse.entity.NotificationType;
-import com.project.Teaming.global.sse.repository.NotificationRepository;
-import com.project.Teaming.global.sse.service.SseEmitterService;
+import com.project.Teaming.global.kafka.producer.NotificationKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class NotificationEventListener {
 
-    private final NotificationRepository notificationRepository;
-    private final SseEmitterService sseEmitterService;
-    private final Executor notificationExecutor;
+    private final NotificationKafkaProducer notificationKafkaProducer;
 
-    @Async
-    @TransactionalEventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void sendNotificationsAfterTransaction(NotificationEvent event) {
-        log.info("✅ 트랜잭션 종료 후 알림 전송 시작: {}",event.getNotificationIds());
+        log.info("✅ 트랜잭션 종료 후 Kafka 알림 이벤트 발행: {}", event.getNotificationIds());
 
-        // ID 기반으로 Notification 객체 조회
-        List<Notification> notifications = notificationRepository.findAllById(event.getNotificationIds());
-
-        notifications.forEach(notification -> {
-            if (shouldProcessMultiThreading(notification)) {
-                CompletableFuture.runAsync(() -> sendWithMultiThreading(notification), notificationExecutor);
-            } else {
-                sendWithoutMultiThreading(notification);
-            }
-        });
-        log.info("✅ 트랜잭션 종료 후 알림 전송 완료!");
-    }
-
-    private boolean shouldProcessMultiThreading(Notification notification) {
-        return
-                (notification.getType().equals(NotificationType.MENTORING_EXPORT.getTitle()) || notification.getType().equals(NotificationType.MENTORING_DELETE.getTitle())
-                || notification.getType().equals(NotificationType.PROJECT_TEAM_QUIT.getTitle()) || notification.getType().equals(NotificationType.PROJECT_TEAM_EXPORT.getTitle()));
-    }
-
-    private void sendWithMultiThreading(Notification notification) {
-        try {
-            sseEmitterService.sendWithTeamIdWithOutAsync(
-                    notification.getUser().getId(),
-                    EventWithTeamPayload.builder()
-                            .userId(notification.getUser().getId())
-                            .type(notification.getType())
-                            .teamId(notification.getTeamId())
-                            .createdAt(notification.getCreatedAt().toString())
-                            .message(notification.getMessage())
-                            .isRead(notification.isRead())
-                            .build());
-        } catch (Exception e) {
-            log.error("❌ SSE 알림 전송 실패 : {}", e.getMessage(), e);
-        }
-    }
-
-    private void sendWithoutMultiThreading(Notification notification) {
-        try {
-            if (notification.getTeamId() != null) {
-                sseEmitterService.sendWithTeamId(
-                        notification.getUser().getId(),
-                        EventWithTeamPayload.builder()
-                                .userId(notification.getUser().getId())
-                                .type(notification.getType())
-                                .teamId(notification.getTeamId())
-                                .createdAt(notification.getCreatedAt().toString())
-                                .message(notification.getMessage())
-                                .isRead(notification.isRead())
-                                .build());
-            } else {
-                sseEmitterService.send(notification.getUser().getId(),
-                        EventPayload.builder()
-                                .userId(notification.getUser().getId())
-                                .type(notification.getType())
-                                .createdAt(notification.getCreatedAt().toString())
-                                .message(notification.getMessage())
-                                .isRead(notification.isRead())
-                                .build());
-            }
-        } catch (Exception e) {
-            log.error("❌ SSE 알림 전송 실패 : {}", e.getMessage(), e);
-        }
+        // 트랜잭션이 성공적으로 커밋된 후 Kafka로 이벤트 발행
+        notificationKafkaProducer.sendNotificationEvent(event);
     }
 }
