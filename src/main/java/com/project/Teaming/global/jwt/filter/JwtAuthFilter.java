c3449445d8f328b4ee9de7c4250e1ff2288243c5
minseo <gwonm305@gmail.com>
2025-02-24 19:24:50 +0900

    fix: 로그 생성

--- src/main/java/com/project/Teaming/global/jwt/filter/JwtAuthFilter.java
+++ src/main/java/com/project/Teaming/global/jwt/filter/JwtAuthFilter.java
@@ -45,7 +45,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String path = request.getRequestURI();
-        log.info("🛑 JWT 필터 실행 - 요청 경로: {}", path);
+        String userAgent = request.getHeader("User-Agent");
+        log.info("🛑 JWT 필터 실행 - 요청 경로: {}, User-Agent: {}", path, userAgent);
 
 
         // Refresh Token 요청은 필터에서 제외
