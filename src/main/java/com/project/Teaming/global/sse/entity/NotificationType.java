e26fc04bca27233abc9dfe8880f832e950821613
minseo <gwonm305@gmail.com>
2025-03-09 16:17:12 +0900

    refactor: 알림 카테고리명 수정

--- src/main/java/com/project/Teaming/global/sse/entity/NotificationType.java
+++ src/main/java/com/project/Teaming/global/sse/entity/NotificationType.java
@@ -7,17 +7,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum NotificationType {
 
-    TEAM_JOIN_REQUEST("프로젝트","프로젝트"),
-    PROJECT_TEAM_ACCEPT("프로젝트","프로젝트"),
-    PROJECT_TEAM_REJECT("프로젝트","프로젝트"),
-    PROJECT_TEAM_QUIT("프로젝트","프로젝트"),
-    PROJECT_TEAM_EXPORT("프로젝트","프로젝트"),
-    MENTORING_TEAM_JOIN_REQUEST("멘토링 팀 신청 알림","멘토링"),
-    MENTORING_TEAM_ACCEPT("멘토링 수락 알림","멘토링"),
-    MENTORING_TEAM_REJECT("멘토링 거절 알림","멘토링"),
-    MENTORING_EXPORT("멘토링 강퇴 알림","멘토링"),
-    MENTORING_EXPORT2("멘토링 강퇴된 유저에게 가는 알림","멘토링"),
-    MENTORING_DELETE("멘토링 탈퇴 알림","멘토링"),
+    TEAM_JOIN_REQUEST("프로젝트","project"),
+    PROJECT_TEAM_ACCEPT("프로젝트","project"),
+    PROJECT_TEAM_REJECT("프로젝트","project"),
+    PROJECT_TEAM_QUIT("프로젝트","project"),
+    PROJECT_TEAM_EXPORT("프로젝트","project"),
+    MENTORING_TEAM_JOIN_REQUEST("멘토링 팀 신청 알림","mentoring"),
+    MENTORING_TEAM_ACCEPT("멘토링 수락 알림","mentoring"),
+    MENTORING_TEAM_REJECT("멘토링 거절 알림","mentoring"),
+    MENTORING_EXPORT("멘토링 강퇴 알림","mentoring"),
+    MENTORING_EXPORT2("멘토링 강퇴된 유저에게 가는 알림","mentoring"),
+    MENTORING_DELETE("멘토링 탈퇴 알림","mentoring"),
     WARNING_COUNT_INCREMENT("warning","전역"),
     WELCOME_USER("notice","전역"),
     WARNING("warning","전역");
